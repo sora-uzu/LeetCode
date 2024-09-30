1585a868b1ee52c6156371c5eac2cffef7ae7062
sora_uzu <naruto07170717@gmail.com>
2024-09-30 17:39:46 +0900

    Repeated String

---
+++ HackerRank/Repeated String.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'repeatedString' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. LONG_INTEGER n
+#
+
+def repeatedString(s, n):
+    afullCount = s.count('a') * (n // len(s))
+    aPartialCount = 0
+
+    for i in range(n % len(s)):
+        if s[i] == 'a':
+            aPartialCount += 1
+
+    return afullCount + aPartialCount
+       
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    n = int(input().strip())
+
+    result = repeatedString(s, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
